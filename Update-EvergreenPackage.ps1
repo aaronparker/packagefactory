bfe633e19cf0f44858762380fff4731ae969173f
Aaron Parker <aaron@stealthpuppy.com>
2021-11-13 12:34:22 +1100

    Working update

--- Update-EvergreenPackage.ps1
+++ Update-EvergreenPackage.ps1
@@ -18,29 +18,32 @@ param (
 $ApplicationList = Get-Content -Path $(Join-Path -Path $Path -ChildPath $Manifest) | ConvertFrom-Json
 
 # Walk through the list of applications
-ForEach ($Application in $ApplicationList.Applications | Get-Member -MemberType "NoteProperty") {
+ForEach ($Application in $ApplicationList.Applications) <#| Get-Member -MemberType "NoteProperty")#> {
     
     # Determine the application download and version number via Evergreen
-    $Properties = $ApplicationList.Applications.($Application.Name)
-    $Evergreen = Invoke-Expression -Command $Properties.Filter
+    #$Properties = $ApplicationList.Applications.($Application.Name)
+    $Evergreen = Invoke-Expression -Command $Application.Filter
+
+    Write-Host "Found: $($Application.Title) $($Evergreen.Version) $($Evergreen.Architecture)."
+
+    # Get the application package manifest and update it
+    $AppConfiguration = $([System.IO.Path]::Combine($Path, $Application.Name, $AppManifest))
+    $AppJson = Get-Content -Path $AppConfiguration | ConvertFrom-Json
 
     # If the version that Evergreen returns is higher than the version in the manifest
-    If ([System.Version]$Evergreen.Version -gt [System.Version]$AppJson.PackageInformation.Version -or [System.String]::IsNullOrEmpty($AppJson.PackageInformation.Version)) {
-
-        # Get the application package manifest and update it
-        $AppConfiguration = $([System.IO.Path]::Combine($Path, $Application.Name, $AppManifest))
-        $AppJson = Get-Content -Path $AppConfiguration | ConvertFrom-Json
+    If ([System.Version]$Evergreen.Version -ge [System.Version]$AppJson.PackageInformation.Version -or [System.String]::IsNullOrEmpty($AppJson.PackageInformation.Version)) {
     
         # Update the manifest with the application setup file
         # TODO: some applications may require unpacking the installer
+        $AppJson.PackageInformation.Version = $Evergreen.Version
         $AppJson.PackageInformation.SetupFile = $(Split-Path -Path $Evergreen.URI -Leaf)
 
         # Update the application display name
-        If ([System.Boolean]($Evergreen.PSobject.Properties.name -match "Architecture")) {
-            $AppJson.Information.DisplayName = "$($Application.AppTitle) $($Evergreen.Version) $($Evergreen.Architecture)"
+        If ([System.Boolean]($Evergreen.PSobject.Properties.Name -match "Architecture")) {
+            $AppJson.Information.DisplayName = "$($Application.Title) $($Evergreen.Version) $($Evergreen.Architecture)"
         }
         Else {
-            $AppJson.Information.DisplayName = "$($Application.AppTitle) $($Evergreen.Version)"
+            $AppJson.Information.DisplayName = "$($Application.Title) $($Evergreen.Version)"
         }
     
         # Step through each DetectionRule to update version properties
@@ -62,7 +65,7 @@ ForEach ($Application in $ApplicationList.Applications | Get-Member -MemberType 
 
         # TODO: Call Create-Win32App.ps1 here
     }
-    ElseIf ([System.Version]$Evergreen.Version -le [System.Version]$AppJson.PackageInformation.Version) {
+    ElseIf ([System.Version]$Evergreen.Version -lt [System.Version]$AppJson.PackageInformation.Version) {
         Write-Host -Object "$($Evergreen.Version) less than or equal to $($AppJson.PackageInformation.Version)."
     }
     Else {
