a728876e1dd206e48bbd6112f7519f2c3c1adae9
Aaron Parker <aaron@stealthpuppy.com>
2022-05-10 19:24:19 +1000

    Update application install details

---
+++ packages/VideoLanVlcPlayer/Source/Install.ps1
@@ -0,0 +1,32 @@
+<#
+    .SYNOPSIS
+    Installs the application
+#>
+[CmdletBinding()]
+Param ()
+
+try {
+    $Installer = Get-ChildItem -Path $PWD -Filter "vlc*.msi" -Recurse -ErrorAction "SilentlyContinue"
+    $Transform = Get-ChildItem -Path $PWD -Filter "VlcPlayerTransform.mst" -Recurse -ErrorAction "SilentlyContinue"
+    $params = @{
+        FilePath     = "$Env:SystemRoot\System32\msiexec.exe"
+        ArgumentList = "/package $($Installer.FullName) /t $($Transform.FullName) ALLUSERS=1 /quiet"
+        NoNewWindow  = $True
+        PassThru     = $True
+        Wait         = $True
+    }
+    $result = Start-Process @params
+}
+catch {
+    throw "Failed to install VLC media player."
+}
+finally {
+    If ($result.ExitCode -eq 0) {
+        $Files = @("$env:ProgramData\Microsoft\Windows\Start Menu\Programs\VideoLAN\VLC\VideoLAN website.lnk",
+            "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\VideoLAN\VLC\Release Notes.lnk",
+            "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\VideoLAN\VLC\Documentation.lnk",
+            "$env:PUBLIC\Desktop\VLC media player.lnk")
+        Remove-Item -Path $Files -Force -ErrorAction "SilentlyContinue"
+    }
+    exit $result.ExitCode
+}
