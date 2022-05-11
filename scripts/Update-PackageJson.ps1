562f1eb0f31d0baf15588c56f27516bdea773426
Aaron Parker <aaron@stealthpuppy.com>
2022-05-11 15:04:02 +1000

    Update for VcRedist

--- scripts/Update-PackageJson.ps1
+++ scripts/Update-PackageJson.ps1
@@ -25,14 +25,14 @@ catch {
 }
 
 # Walk through the list of applications
-foreach ($Application in $ApplicationList.Applications) {
+foreach ($Application in $ApplicationList) {
 
     # Determine the application download and version number via Evergreen
     #$Properties = $ApplicationList.Applications.($Application.Name)
     Write-Host -ForegroundColor "Cyan" "Application: $($Application.Title)"
     Write-Host -ForegroundColor "Cyan" "Running: $($Application.Filter)."
-    $Evergreen = Invoke-Expression -Command $Application.Filter
-    Write-Host -ForegroundColor "Cyan" "Found: $($Application.Title) $($Evergreen.Version) $($Evergreen.Architecture)."
+    $AppUpdate = Invoke-Expression -Command $Application.Filter
+    Write-Host -ForegroundColor "Cyan" "Found: $($Application.Title) $($AppUpdate.Version) $($AppUpdate.Architecture)."
 
     # Get the application package manifest and update it
     $AppConfiguration = $([System.IO.Path]::Combine($Path, $Application.Name, $AppManifest))
@@ -40,36 +40,44 @@ foreach ($Application in $ApplicationList.Applications) {
     $AppJson = Get-Content -Path $AppConfiguration | ConvertFrom-Json
 
     # If the version that Evergreen returns is higher than the version in the manifest
-    if ([System.Version]$Evergreen.Version -ge [System.Version]$AppJson.PackageInformation.Version -or [System.String]::IsNullOrEmpty($AppJson.PackageInformation.Version)) {
+    if ([System.Version]$AppUpdate.Version -ge [System.Version]$AppJson.PackageInformation.Version -or [System.String]::IsNullOrEmpty($AppJson.PackageInformation.Version)) {
 
         # Update the manifest with the application setup file
         # TODO: some applications may require unpacking the installer
         Write-Host -ForegroundColor "Cyan" "Update package."
-        $AppJson.PackageInformation.Version = $Evergreen.Version
-        $AppJson.PackageInformation.SetupFile = $(Split-Path -Path $Evergreen.URI -Leaf) -replace "%20", " "
-        $AppJson.Program.InstallCommand = $AppJson.Program.InstallTemplate -replace "#SetupFile", $(Split-Path -Path $Evergreen.URI -Leaf) -replace "%20", " "
+        $AppJson.PackageInformation.Version = $AppUpdate.Version
+        $AppJson.PackageInformation.SetupFile = $(Split-Path -Path $AppUpdate.URI -Leaf) -replace "%20", " "
+        $AppJson.Program.InstallCommand = $AppJson.Program.InstallTemplate -replace "#SetupFile", $(Split-Path -Path $AppUpdate.URI -Leaf) -replace "%20", " "
+
+        If ([System.Boolean]($AppUpdate.PSobject.Properties.Name -match "SilentUninstall")) {
+            $AppJson.Program.UninstallCommand = $AppUpdate.SilentUninstall -replace "%ProgramData%", "C:\ProgramData"
+        }
 
         # Update the application display name
-        if ([System.Boolean]($Evergreen.PSobject.Properties.Name -match "Architecture")) {
-            $AppJson.Information.DisplayName = "$($Application.Title) $($Evergreen.Version) $($Evergreen.Architecture)"
+        if ([System.Boolean]($AppUpdate.PSobject.Properties.Name -match "Architecture")) {
+            $AppJson.Information.DisplayName = "$($Application.Title) $($AppUpdate.Version) $($AppUpdate.Architecture)"
         }
         else {
-            $AppJson.Information.DisplayName = "$($Application.Title) $($Evergreen.Version)"
+            $AppJson.Information.DisplayName = "$($Application.Title) $($AppUpdate.Version)"
         }
 
         # Step through each DetectionRule to update version properties
         for ($i = 0; $i -le $AppJson.DetectionRule.Count - 1; $i++) {
 
             if ("Value" -in ($AppJson.DetectionRule[$i] | Get-Member -MemberType "NoteProperty" | Select-Object -ExpandProperty "Name")) {
-                $AppJson.DetectionRule[$i].Value = $Evergreen.Version
+                $AppJson.DetectionRule[$i].Value = $AppUpdate.Version
             }
 
             if ("VersionValue" -in ($AppJson.DetectionRule[$i] | Get-Member -MemberType "NoteProperty" | Select-Object -ExpandProperty "Name")) {
-                $AppJson.DetectionRule[$i].VersionValue = $Evergreen.Version
+                $AppJson.DetectionRule[$i].VersionValue = $AppUpdate.Version
             }
 
             if ("ProductVersion" -in ($AppJson.DetectionRule[$i] | Get-Member -MemberType "NoteProperty" | Select-Object -ExpandProperty "Name")) {
-                $AppJson.DetectionRule[$i].ProductVersion = $Evergreen.Version
+                $AppJson.DetectionRule[$i].ProductVersion = $AppUpdate.Version
+            }
+
+            if ("ProductCode" -in ($AppJson.DetectionRule[$i] | Get-Member -MemberType "NoteProperty" | Select-Object -ExpandProperty "Name")) {
+                $AppJson.DetectionRule[$i].ProductVersion = $AppUpdate.ProductCode
             }
         }
 
@@ -77,8 +85,8 @@ foreach ($Application in $ApplicationList.Applications) {
         Write-Host -ForegroundColor "Cyan" "Output: $AppConfiguration."
         $AppJson | ConvertTo-Json | Out-File -Path $AppConfiguration -Force
     }
-    elseif ([System.Version]$Evergreen.Version -lt [System.Version]$AppJson.PackageInformation.Version) {
-        Write-Host -ForegroundColor "Cyan" "$($Evergreen.Version) less than or equal to $($AppJson.PackageInformation.Version)."
+    elseif ([System.Version]$AppUpdate.Version -lt [System.Version]$AppJson.PackageInformation.Version) {
+        Write-Host -ForegroundColor "Cyan" "$($AppUpdate.Version) less than or equal to $($AppJson.PackageInformation.Version)."
     }
     else {
         Write-Host -ForegroundColor "Cyan" "Could not compare package version between Evergreen and the application manifest."
