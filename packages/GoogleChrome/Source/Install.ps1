2c63a92542b03932bf5f1e4a4efb5f3230dd25b6
Aaron Parker <aaron@stealthpuppy.com>
2022-05-13 20:27:05 +1000

    Update Install.ps1

--- packages/GoogleChrome/Source/Install.ps1
+++ packages/GoogleChrome/Source/Install.ps1
@@ -5,6 +5,40 @@
 [CmdletBinding()]
 Param ()
 
+$prefs = @"
+{
+    "homepage": "https://www.office.com",
+    "homepage_is_newtabpage": false,
+    "browser": {
+        "show_home_button": true
+    },
+    "session": {
+        "restore_on_startup": 4,
+        "startup_urls": []
+    },
+    "bookmark_bar": {
+        "show_on_all_tabs": false
+    },
+    "sync_promo": {
+        "show_on_first_run_allowed": false
+    },
+    "distribution": {
+        "ping_delay": 60,
+        "suppress_first_run_bubble": true,
+        "do_not_create_desktop_shortcut": true,
+        "do_not_create_quick_launch_shortcut": true,
+        "do_not_launch_chrome": true,
+        "do_not_register_for_update_launch": true,
+        "make_chrome_default": false,
+        "make_chrome_default_for_user": false,
+        "suppress_first_run_default_browser_prompt": true,
+        "system_level": true,
+        "verbose_logging": true
+    },
+    "first_run_tabs": []
+}
+"@
+
 try {
     $Installer = Get-ChildItem -Path $PWD -Filter "googlechromestandaloneenterprise64.msi" -Recurse -ErrorAction "SilentlyContinue"
     $params = @{
@@ -16,9 +50,15 @@ try {
     }
     $result = Start-Process @params
 
-    # Copy the initial_preferences file
-    $File = Get-ChildItem -Path $PWD -Filter "initial_preferences.txt" -Recurse -ErrorAction "SilentlyContinue"
-    Copy-Item -Path $File.FullName -Destination "$Env:ProgramFiles\Google\Chrome\Application\initial_preferences" -Force -ErrorAction "SilentlyContinue" | Out-Null
+    # Create the initial_preferences file
+    $params = @{
+        FilePath    = "$Env:ProgramFiles\Google\Chrome\Application\initial_preferences"
+        Encoding    = "Utf8"
+        Force       = $True
+        NoNewline   = $True
+        ErrorAction = "SilentlyContinue"
+    }
+    $prefs | Out-File @params
 }
 catch {
     throw "Failed to install Google Chrome."
