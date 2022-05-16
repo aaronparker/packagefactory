36666788b212c20e7f6e018a092500fb497e704b
Aaron Parker <aaron@stealthpuppy.com>
2022-05-16 20:34:06 +1000

    Update Install.ps1

--- packages/MicrosoftPowerToys/Source/Install.ps1
+++ packages/MicrosoftPowerToys/Source/Install.ps1
@@ -6,10 +6,11 @@
 param ()
 
 try {
+    New-Item -Path "$env:ProgramData\PackageFactory\Logs" -ItemType "Directory" -ErrorAction "SilentlyContinue" | Out-Null
     $Installer = Get-ChildItem -Path $PWD -Filter "PowerToysSetup*.exe" -Recurse -ErrorAction "SilentlyContinue"
     $params = @{
         FilePath     = $Installer.FullName
-        ArgumentList = "/install /quiet /norestart"
+        ArgumentList = "/install /quiet /norestart /log $env:ProgramData\PackageFactory\Logs\MicrosoftPowerToys.log"
         NoNewWindow  = $True
         PassThru     = $True
         Wait         = $True
