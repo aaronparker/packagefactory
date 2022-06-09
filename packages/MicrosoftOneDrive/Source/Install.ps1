5e2cec5bc08d0a4157856f077b3841367d3b2ada
Aaron Parker <aaron@stealthpuppy.com>
2022-06-09 11:33:29 +1000

    Add `MicrosoftOneDrive`; `Move Applications.json`

---
+++ packages/MicrosoftOneDrive/Source/Install.ps1
@@ -0,0 +1,54 @@
+<#
+    .SYNOPSIS
+    Installs the application
+#>
+[CmdletBinding()]
+param ()
+
+#region Restart if running in a 32-bit session
+If (!([System.Environment]::Is64BitProcess)) {
+    If ([System.Environment]::Is64BitOperatingSystem) {
+        $Arguments = "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$($MyInvocation.MyCommand.Definition)`""
+        $ProcessPath = $(Join-Path -Path $Env:SystemRoot -ChildPath "\Sysnative\WindowsPowerShell\v1.0\powershell.exe")
+        Write-Verbose -Message "Restarting in 64-bit PowerShell."
+        Write-Verbose -Message "FilePath: $ProcessPath."
+        Write-Verbose -Message "Arguments: $Arguments."
+        $params = @{
+            FilePath     = $ProcessPath
+            ArgumentList = $Arguments
+            Wait         = $True
+            WindowStyle  = "Hidden"
+        }
+        Start-Process @params
+        Exit 0
+    }
+}
+#endregion
+
+try {
+    Get-Process -ErrorAction "SilentlyContinue" | `
+        Where-Object { $_.Path -like "$env:ProgramFiles\Microsoft OneDrive\*" } | `
+        Stop-Process -Force -ErrorAction "SilentlyContinue"
+}
+catch {
+    Write-Warning -Message "Failed to stop OneDrive processes."
+}
+
+try {
+    New-Item -Path "$env:ProgramData\PackageFactory\Logs" -ItemType "Directory" -ErrorAction "SilentlyContinue" | Out-Null
+    $Installer = Get-ChildItem -Path $PWD -Filter "OneDriveSetup*.exe" -Recurse -ErrorAction "SilentlyContinue"
+    $params = @{
+        FilePath     = $Installer.FullName
+        ArgumentList = "/install /quiet /norestart /log $env:ProgramData\PackageFactory\Logs\MicrosoftOneDrive.log"
+        NoNewWindow  = $True
+        PassThru     = $True
+        Wait         = $True
+    }
+    $result = Start-Process @params
+}
+catch {
+    throw "Failed to install Microsoft OneDrive."
+}
+finally {
+    exit $result.ExitCode
+}
