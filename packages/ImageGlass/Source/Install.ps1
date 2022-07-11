887bd20fa80b75c549932956fc0798132784c779
Aaron Parker <aaron@stealthpuppy.com>
2022-07-11 21:28:11 +1000

    Add ImageGlass

---
+++ packages/ImageGlass/Source/Install.ps1
@@ -0,0 +1,50 @@
+<#
+    .SYNOPSIS
+    Installs the application
+#>
+[CmdletBinding()]
+param ()
+
+#region Restart if running in a 32-bit session
+If (!([System.Environment]::Is64BitProcess)) {
+    If ([System.Environment]::Is64BitOperatingSystem) {
+        $Arguments = "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$($MyInvocation.MyCommand.Definition)`""
+        $ProcessPath = $(Join-Path -Path $Env:SystemRoot -ChildPath "\Sysnative\WindowsPowerShell\v1.0\powershell.exe")
+        Write-Verbose -Message "Restarting in 64-bit PowerShell."
+        Write-Verbose -Message "FilePath: $ProcessPath."
+        Write-Verbose -Message "Arguments: $Arguments."
+        $params = @{
+            FilePath     = $ProcessPath
+            ArgumentList = $Arguments
+            Wait         = $True
+            WindowStyle  = "Hidden"
+        }
+        Start-Process @params
+        Exit 0
+    }
+}
+#endregion
+
+try {
+    $Installer = Get-ChildItem -Path $PWD -Filter "ImageGlass_Kobe_*_x64.msi" -Recurse -ErrorAction "SilentlyContinue"
+    $params = @{
+        FilePath     = "$Env:SystemRoot\System32\msiexec.exe"
+        ArgumentList = "/package `"$($Installer.FullName)`" RUNAPPLICATION=0 ALLUSERS=1 /quiet"
+        NoNewWindow  = $True
+        PassThru     = $True
+        Wait         = $True
+    }
+    $result = Start-Process @params
+}
+catch {
+    throw "Failed to install ImageGlass."
+}
+finally {
+    if ($result.ExitCode -eq 0) {
+        $Files = @("$env:PUBLIC\Desktop\ImageGlass.lnk",
+            "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\ImageGlass\ImageGlass' LICENSE.lnk",
+            "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\ImageGlass\Uninstall ImageGlass*.lnk")
+        Remove-Item -Path $Files -Force -ErrorAction "SilentlyContinue"
+    }
+    exit $result.ExitCode
+}
