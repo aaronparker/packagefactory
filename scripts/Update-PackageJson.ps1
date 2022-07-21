525d1a1e2c96e7c8434eab57687c5a5061c10894
Aaron Parker <aaron@stealthpuppy.com>
2022-07-21 13:58:02 +1000

    Update Update-PackageJson.ps1

--- scripts/Update-PackageJson.ps1
+++ scripts/Update-PackageJson.ps1
@@ -29,92 +29,100 @@ catch {
 foreach ($Application in $ApplicationList) {
 
     # Determine the application download and version number via Evergreen
-    #$Properties = $ApplicationList.Applications.($Application.Name)
     Write-Host -ForegroundColor "Cyan" "Application: $($Application.Title)"
     Write-Host -ForegroundColor "Cyan" "Running: $($Application.Filter)."
-    $AppUpdate = Invoke-Expression -Command $Application.Filter
-    Write-Host -ForegroundColor "Cyan" "Found: $($Application.Title) $($AppUpdate.Version) $($AppUpdate.Architecture)."
 
-    # Get the application package manifest and update it
-    $AppConfiguration = $([System.IO.Path]::Combine($Path, $Application.Name, $AppManifest))
-    Write-Host -ForegroundColor "Cyan" "Read: $AppConfiguration."
-    if (Test-Path -Path $AppConfiguration) {
-        $AppData = Get-Content -Path $AppConfiguration | ConvertFrom-Json
-    }
-    else {
-        Write-Warning -Message "Cannot find: $AppConfiguration."
-    }
+    # Get the details of the application
+    $AppUpdate = Invoke-Expression -Command $Application.Filter -ErrorAction "SilentlyContinue" -WarningAction "SilentlyContinue"
+    if ($Null -ne $AppManifest) {
 
-    # If the version that Evergreen returns is higher than the version in the manifest
-    if ([System.Version]$AppUpdate.Version -ge [System.Version]$AppData.PackageInformation.Version -or [System.String]::IsNullOrEmpty($AppData.PackageInformation.Version)) {
+        Write-Host -ForegroundColor "Cyan" "Found: $($Application.Title) $($AppUpdate.Version) $($AppUpdate.Architecture)."
 
-        # Update the manifest with the application setup file
-        # TODO: some applications may require unpacking the installer
-        Write-Host -ForegroundColor "Cyan" "Update package."
-        $AppData.PackageInformation.Version = $AppUpdate.Version
-
-        if ([System.Boolean]($AppUpdate.PSobject.Properties.Name -match "URI")) {
-            if ($AppUpdate.URI -match "\.zip$") {
-                if (Test-Path -Path Env:Temp -ErrorAction "SilentlyContinue") { $ZipPath = $Env:Temp } else { $ZipPath = $HOME }
-                $Download = $AppUpdate | Save-EvergreenApp -CustomPath $ZipPath
-                [Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
-                $SetupFile = [IO.Compression.ZipFile]::OpenRead($Download.FullName).Entries.FullName
-                Remove-Item -Path $Download.FullName -Force
-
-                $AppData.PackageInformation.SetupFile = $SetupFile -replace "%20", " "
-                $AppData.Program.InstallCommand = $AppData.Program.InstallTemplate -replace "#SetupFile", $SetupFile -replace "%20", " "
-            } else {
-                $AppData.PackageInformation.SetupFile = $(Split-Path -Path $AppUpdate.URI -Leaf) -replace "%20", " "
-                $AppData.Program.InstallCommand = $AppData.Program.InstallTemplate -replace "#SetupFile", $(Split-Path -Path $AppUpdate.URI -Leaf) -replace "%20", " "
-            }
+        # Get the application package manifest and update it
+        $AppConfiguration = $([System.IO.Path]::Combine($Path, $Application.Name, $AppManifest))
+        Write-Host -ForegroundColor "Cyan" "Read: $AppConfiguration."
+        if (Test-Path -Path $AppConfiguration) {
+            $AppData = Get-Content -Path $AppConfiguration | ConvertFrom-Json
         }
         else {
-            $AppData.PackageInformation.SetupFile = $(Split-Path -Path $AppUpdate.Download -Leaf) -replace "%20", " "
-            $AppData.Program.InstallCommand = $AppData.Program.InstallTemplate -replace "#SetupFile", $(Split-Path -Path $AppUpdate.Download -Leaf)
+            Write-Warning -Message "Cannot find: $AppConfiguration."
         }
 
-        if ([System.Boolean]($AppUpdate.PSobject.Properties.Name -match "SilentUninstall")) {
-            $AppData.Program.UninstallCommand = $AppUpdate.SilentUninstall -replace "%ProgramData%", "C:\ProgramData"
-        }
+        # If the version that Evergreen returns is higher than the version in the manifest
+        if ([System.Version]$AppUpdate.Version -ge [System.Version]$AppData.PackageInformation.Version -or [System.String]::IsNullOrEmpty($AppData.PackageInformation.Version)) {
 
-        # Update the application display name
-        if ([System.Boolean]($AppUpdate.PSobject.Properties.Name -match "Architecture")) {
-            $AppData.Information.DisplayName = "$($Application.Title) $($AppUpdate.Version) $($AppUpdate.Architecture)"
-        }
-        else {
-            $AppData.Information.DisplayName = "$($Application.Title) $($AppUpdate.Version)"
-        }
+            # Update the manifest with the application setup file
+            # TODO: some applications may require unpacking the installer
+            Write-Host -ForegroundColor "Cyan" "Update package."
+            $AppData.PackageInformation.Version = $AppUpdate.Version
 
-        # Step through each DetectionRule to update version properties
-        for ($i = 0; $i -le $AppData.DetectionRule.Count - 1; $i++) {
+            if ([System.Boolean]($AppUpdate.PSobject.Properties.Name -match "URI")) {
+                if ($AppUpdate.URI -match "\.zip$") {
+                    if (Test-Path -Path Env:Temp -ErrorAction "SilentlyContinue") { $ZipPath = $Env:Temp } else { $ZipPath = $HOME }
+                    $Download = $AppUpdate | Save-EvergreenApp -CustomPath $ZipPath
+                    [Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem')
+                    $SetupFile = [IO.Compression.ZipFile]::OpenRead($Download.FullName).Entries.FullName
+                    Remove-Item -Path $Download.FullName -Force
 
-            if ("Value" -in ($AppData.DetectionRule[$i] | Get-Member -MemberType "NoteProperty" | Select-Object -ExpandProperty "Name")) {
-                $AppData.DetectionRule[$i].Value = $AppUpdate.Version
-            }
-
-            if ("VersionValue" -in ($AppData.DetectionRule[$i] | Get-Member -MemberType "NoteProperty" | Select-Object -ExpandProperty "Name")) {
-                $AppData.DetectionRule[$i].VersionValue = $AppUpdate.Version
-            }
-
-            if ("ProductVersion" -in ($AppData.DetectionRule[$i] | Get-Member -MemberType "NoteProperty" | Select-Object -ExpandProperty "Name")) {
-                $AppData.DetectionRule[$i].ProductVersion = $AppUpdate.Version
-            }
-
-            if ("ProductCode" -in ($AppData.DetectionRule[$i] | Get-Member -MemberType "NoteProperty" | Select-Object -ExpandProperty "Name")) {
-                if ($Null -ne $AppUpdate.ProductCode) {
-                    $AppData.DetectionRule[$i].ProductCode = $AppUpdate.ProductCode
+                    $AppData.PackageInformation.SetupFile = $SetupFile -replace "%20", " "
+                    $AppData.Program.InstallCommand = $AppData.Program.InstallTemplate -replace "#SetupFile", $SetupFile -replace "%20", " "
+                }
+                else {
+                    $AppData.PackageInformation.SetupFile = $(Split-Path -Path $AppUpdate.URI -Leaf) -replace "%20", " "
+                    $AppData.Program.InstallCommand = $AppData.Program.InstallTemplate -replace "#SetupFile", $(Split-Path -Path $AppUpdate.URI -Leaf) -replace "%20", " "
                 }
             }
-        }
+            else {
+                $AppData.PackageInformation.SetupFile = $(Split-Path -Path $AppUpdate.Download -Leaf) -replace "%20", " "
+                $AppData.Program.InstallCommand = $AppData.Program.InstallTemplate -replace "#SetupFile", $(Split-Path -Path $AppUpdate.Download -Leaf)
+            }
 
-        # Write the application manifest back to disk
-        Write-Host -ForegroundColor "Cyan" "Output: $AppConfiguration."
-        $AppData | ConvertTo-Json | Out-File -FilePath $AppConfiguration -Force
-    }
-    elseif ([System.Version]$AppUpdate.Version -lt [System.Version]$AppData.PackageInformation.Version) {
-        Write-Host -ForegroundColor "Cyan" "$($AppUpdate.Version) less than $($AppData.PackageInformation.Version)."
+            if ([System.Boolean]($AppUpdate.PSobject.Properties.Name -match "SilentUninstall")) {
+                $AppData.Program.UninstallCommand = $AppUpdate.SilentUninstall -replace "%ProgramData%", "C:\ProgramData"
+            }
+
+            # Update the application display name
+            if ([System.Boolean]($AppUpdate.PSobject.Properties.Name -match "Architecture")) {
+                $AppData.Information.DisplayName = "$($Application.Title) $($AppUpdate.Version) $($AppUpdate.Architecture)"
+            }
+            else {
+                $AppData.Information.DisplayName = "$($Application.Title) $($AppUpdate.Version)"
+            }
+
+            # Step through each DetectionRule to update version properties
+            for ($i = 0; $i -le $AppData.DetectionRule.Count - 1; $i++) {
+
+                if ("Value" -in ($AppData.DetectionRule[$i] | Get-Member -MemberType "NoteProperty" | Select-Object -ExpandProperty "Name")) {
+                    $AppData.DetectionRule[$i].Value = $AppUpdate.Version
+                }
+
+                if ("VersionValue" -in ($AppData.DetectionRule[$i] | Get-Member -MemberType "NoteProperty" | Select-Object -ExpandProperty "Name")) {
+                    $AppData.DetectionRule[$i].VersionValue = $AppUpdate.Version
+                }
+
+                if ("ProductVersion" -in ($AppData.DetectionRule[$i] | Get-Member -MemberType "NoteProperty" | Select-Object -ExpandProperty "Name")) {
+                    $AppData.DetectionRule[$i].ProductVersion = $AppUpdate.Version
+                }
+
+                if ("ProductCode" -in ($AppData.DetectionRule[$i] | Get-Member -MemberType "NoteProperty" | Select-Object -ExpandProperty "Name")) {
+                    if ($Null -ne $AppUpdate.ProductCode) {
+                        $AppData.DetectionRule[$i].ProductCode = $AppUpdate.ProductCode
+                    }
+                }
+            }
+
+            # Write the application manifest back to disk
+            Write-Host -ForegroundColor "Cyan" "Output: $AppConfiguration."
+            $AppData | ConvertTo-Json | Out-File -FilePath $AppConfiguration -Force
+        }
+        elseif ([System.Version]$AppUpdate.Version -lt [System.Version]$AppData.PackageInformation.Version) {
+            Write-Host -ForegroundColor "Cyan" "$($AppUpdate.Version) less than $($AppData.PackageInformation.Version)."
+        }
+        else {
+            Write-Host -ForegroundColor "Cyan" "Could not compare package version."
+        }
     }
     else {
-        Write-Host -ForegroundColor "Cyan" "Could not compare package version."
+        Write-Host "Failed to return details from: $($Application.Filter )"
     }
 }
