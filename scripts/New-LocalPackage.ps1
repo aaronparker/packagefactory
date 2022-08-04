2b3fa75203b8d3768bef4ae57c3f049a2fcce0d1
Aaron Parker <aaron@stealthpuppy.com>
2022-08-04 13:04:40 +1000

    Update New-LocalPackage.ps1

--- scripts/New-LocalPackage.ps1
+++ scripts/New-LocalPackage.ps1
@@ -1,23 +1,41 @@
-$Application = "MicrosoftVcRedist2022x86"
-$Apps = Get-Content -Path "E:\Temp\packagefactory\Applications.json" | ConvertFrom-Json
-$Filter = ($Apps | Where-Object { $_.Name -eq "$Application" }).Filter
-if ($Filter -match "Get-VcList") {
-    $App = Invoke-Expression -Command $Filter
-    $Filename = $(Split-Path -Path $App.Download -Leaf)
-    Write-Host "Package: $($App.Name); $Filename."
-    New-Item -Path "E:\Temp\packagefactory\packages\$Application\Source" -ItemType "Directory" -Force | Out-Null
-    Invoke-WebRequest -Uri $App.Download -OutFile "E:\Temp\packagefactory\packages\$Application\Source\$Filename" -UseBasicParsing
-} else {
-    $result = Invoke-Expression -Command $Filter | Save-EvergreenApp -CustomPath "E:\Temp\packagefactory\packages\$Application\Source"
-    If ($result.FullName -match "\.zip$") {
-        Expand-Archive -DestinationPath "E:\Temp\packagefactory\packages\$Application\Source"
-        Remove-Item -Path $result.FullName -Force
-    }
-}
+#Requires -Modules Evergreen, VcRedist
+<#
+    Import application packages into Intune locally
+#>
+[CmdletBinding()]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingInvokeExpression", "")]
+param (
+    [Parameter()]
+    [System.String] $Path = "E:\projects\packagefactory",
 
-$params = @{
-    Application       = $Application
-    Path              = "E:\Temp\packagefactory\packages"
-    DisplayNameSuffix = ""
+    [Parameter()]
+    [System.String[]] $Applications = @("MicrosoftVcRedist2022x86", "MicrosoftVcRedist2022x64", "AdobeAcrobatReaderDC")
+)
+
+foreach ($Application in $Applications) {
+
+    $Apps = Get-Content -Path "$Path\Applications.json" | ConvertFrom-Json
+    $Filter = ($Apps | Where-Object { $_.Name -eq "$Application" }).Filter
+    if ($Filter -match "Get-VcList") {
+        $App = Invoke-Expression -Command $Filter
+        $Filename = $(Split-Path -Path $App.Download -Leaf)
+        Write-Host "Package: $($App.Name); $Filename."
+        New-Item -Path "$Path\packages\$Application\Source" -ItemType "Directory" -Force | Out-Null
+        Invoke-WebRequest -Uri $App.Download -OutFile "$Path\packages\$Application\Source\$Filename" -UseBasicParsing
+    }
+    else {
+        $result = Invoke-Expression -Command $Filter | Save-EvergreenApp -CustomPath "$Path\packages\$Application\Source"
+        if ($result.FullName -match "\.zip$") {
+            Expand-Archive -DestinationPath "$Path\packages\$Application\Source"
+            Remove-Item -Path $result.FullName -Force
+        }
+    }
+
+    $params = @{
+        Application       = $Application
+        Path              = "$Path\packages"
+        DisplayNameSuffix = ""
+    }
+    .\Create-Win32App.ps1 @params
 }
-.\Create-Win32App.ps1 @params
