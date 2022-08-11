017b11db674aac4fba9b82849579d6590a8a75ca
Aaron Parker <aaron@stealthpuppy.com>
2022-08-11 17:02:51 +1000

    Add MicrosoftPowerShell

---
+++ packages/MicrosoftPowerShell/Source/Uninstall.ps1
@@ -0,0 +1,44 @@
+<#
+    .SYNOPSIS
+    Uninstalls the application
+#>
+[CmdletBinding()]
+param ()
+
+#region Restart if running in a 32-bit session
+if (!([System.Environment]::Is64BitProcess)) {
+    if ([System.Environment]::Is64BitOperatingSystem) {
+        $Arguments = "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$($MyInvocation.MyCommand.Definition)`""
+        $ProcessPath = $(Join-Path -Path $Env:SystemRoot -ChildPath "\Sysnative\WindowsPowerShell\v1.0\powershell.exe")
+        Write-Verbose -Message "Restarting in 64-bit PowerShell."
+        Write-Verbose -Message "FilePath: $ProcessPath."
+        Write-Verbose -Message "Arguments: $Arguments."
+        $params = @{
+            FilePath     = $ProcessPath
+            ArgumentList = $Arguments
+            Wait         = $True
+            WindowStyle  = "Hidden"
+        }
+        Start-Process @params
+        exit 0
+    }
+}
+#endregion
+
+try {
+    $Chrome = Get-CimInstance -Class "Win32_Product" | Where-Object { $_.Caption -like "PowerShell 7-x64" }
+    $params = @{
+        FilePath     = "$Env:SystemRoot\System32\msiexec.exe"
+        ArgumentList = "/uninstall `"$($Chrome.IdentifyingNumber)`" /quiet"
+        NoNewWindow  = $True
+        PassThru     = $True
+        Wait         = $True
+    }
+    $result = Start-Process @params
+}
+catch {
+    throw "Failed to uninstall Microsoft PowerShell."
+}
+finally {
+    exit $result.ExitCode
+}
