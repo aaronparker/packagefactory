2d6ec046442017ca2e9857135f2a68fbb1e67498
Aaron Parker <aaron@stealthpuppy.com>
2022-08-17 08:27:20 +1000

    ReincubateCamo

---
+++ packages/ReincubateCamo/Source/Install.ps1
@@ -0,0 +1,49 @@
+<#
+    .SYNOPSIS
+    Installs the application
+#>
+[CmdletBinding()]
+param ()
+
+#region Restart if running in a 32-bit session
+if (!([System.Environment]::Is64BitProcess)) {
+    if ([System.Environment]::Is64BitOperatingSystem) {
+        $Arguments = "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$($MyInvocation.MyCommand.Definition)`""
+        $ProcessPath = $(Join-Path -Path $Env:SystemRoot -ChildPath "\Sysnative\WindowsPowerShell\v1.0\powershell.exe")
+        Write-Verbose -Message "Restarting in 64-bit PowerShell."
+        Write-Verbose -Message "FilePath: $ProcessPath."
+        Write-Verbose -Message "Arguments: $Arguments."
+        $params = @{
+            FilePath     = $ProcessPath
+            ArgumentList = $Arguments
+            Wait         = $True
+            WindowStyle  = "Hidden"
+        }
+        Start-Process @params
+        exit 0
+    }
+}
+#endregion
+
+try {
+    New-Item -Path "$env:ProgramData\PackageFactory\Logs" -ItemType "Directory" -ErrorAction "SilentlyContinue" | Out-Null
+    $Installer = Get-ChildItem -Path $PWD -Filter "camo-windows-latest.msi" -Recurse -ErrorAction "SilentlyContinue"
+    $params = @{
+        FilePath     = "$Env:SystemRoot\System32\msiexec.exe"
+        ArgumentList = "/package `"$($Installer.FullName)`" ALLUSERS=1 /quiet /log `"C:\ProgramData\PackageFactory\logs\ReincubateCamo.log`""
+        NoNewWindow  = $True
+        PassThru     = $True
+        Wait         = $True
+    }
+    $result = Start-Process @params
+}
+catch {
+    throw "Failed to install Reincubate Camo."
+}
+finally {
+    if ($result.ExitCode -eq 0) {
+        $Files = @("$env:PUBLIC\Desktop\Camo Studio.lnk")
+        Remove-Item -Path $Files -Force -ErrorAction "SilentlyContinue"
+    }
+    exit $result.ExitCode
+}
