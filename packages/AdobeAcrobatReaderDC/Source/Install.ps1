8c0e04408814b5e34bf5c0676da4a53e12bbb4e8
Aaron Parker <aaron@stealthpuppy.com>
2022-08-19 15:26:03 +1000

    Install.json

--- packages/AdobeAcrobatReaderDC/Source/Install.ps1
+++ packages/AdobeAcrobatReaderDC/Source/Install.ps1
@@ -22,21 +22,76 @@ if (!([System.Environment]::Is64BitProcess)) {
 }
 #endregion
 
-try {
-    New-Item -Path "$env:ProgramData\PackageFactory\Logs" -ItemType "Directory" -ErrorAction "SilentlyContinue" | Out-Null
-    $Installer = Get-ChildItem -Path $PWD -Filter "AcroRdrDC*.exe" -Recurse -ErrorAction "SilentlyContinue"
-    $params = @{
-        FilePath     = $Installer.FullName
-        ArgumentList = "-sfx_nu /sALL /rps /l /msi EULA_ACCEPT=YES ENABLE_CHROMEEXT=0 DISABLE_BROWSER_INTEGRATION=1 ENABLE_OPTIMIZATION=YES ADD_THUMBNAILPREVIEW=0 DISABLEDESKTOPSHORTCUT=1 /log `"$env:ProgramData\PackageFactory\logs\AdobeAcrobatReaderDC.log`""
-        NoNewWindow  = $True
-        PassThru     = $True
-        Wait         = $True
+#region Functions
+function Get-InstallConfig {
+    try {
+        $Path = Join-Path -Path $PWD -ChildPath "Install.json"
+        Get-Content -Path $Path -ErrorAction "SilentlyContinue" | ConvertFrom-Json -ErrorAction "SilentlyContinue"
     }
-    $result = Start-Process @params
+    catch {
+        throw $_
+    }
 }
-catch {
-    throw $_
+
+function Get-Installer ($File) {
+    $Installer = Get-ChildItem -Path $PWD -Filter $File -Recurse -ErrorAction "SilentlyContinue" | Select-Object -First 1
+    if ([System.String]::IsNullOrEmpty($Installer)) {
+        throw "File not found: $File"
+    }
+    else {
+        return $Installer.FullName
+    }
 }
-finally {
-    exit $result.ExitCode
+#region
+
+# Get the install details for this application
+$Install = Get-InstallConfig
+$Installer = Get-Installer -File $Install.PackageInformation.SetupFile
+if ([System.String]::IsNullOrEmpty($Installer)) {
+
+    # Create the log folder
+    New-Item -Path $Install.LogPath -ItemType "Directory" -ErrorAction "SilentlyContinue" | Out-Null
+
+    # Build the argument list
+    $ArgumentList = $Install.InstallTasks.ArgumentList -replace "#SetupFile", $Installer
+    $ArgumentList = $ArgumentList -replace "#LogName", $Install.PackageInformation.SetupFile
+    $ArgumentList = $ArgumentList -replace "#LogPath", $Install.LogPath
+
+    try {
+        switch ($Install.PackageInformation.SetupType) {
+            "EXE" {
+                $params = @{
+                    FilePath     = $Installer
+                    ArgumentList = $ArgumentList
+                    NoNewWindow  = $True
+                    PassThru     = $True
+                    Wait         = $True
+                }
+                $result = Start-Process @params
+            }
+            "MSI" {
+                $params = @{
+                    FilePath     = "$Env:SystemRoot\System32\msiexec.exe"
+                    ArgumentList = $ArgumentList
+                    NoNewWindow  = $True
+                    PassThru     = $True
+                    Wait         = $True
+                }
+                $result = Start-Process @params
+            }
+            default {
+                exit 1
+            }
+        }
+    }
+    catch {
+        throw $_
+    }
+    finally {
+        Remove-Item -Path $Install.PostInstall.Remove -Force -ErrorAction "SilentlyContinue"
+        exit $result.ExitCode
+    }
+}
+else {
+    exit 1
 }
