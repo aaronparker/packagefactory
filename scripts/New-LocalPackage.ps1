57d97becfecab4881f69352eb7e7b2e2beb4e8eb
Aaron Parker <aaron@stealthpuppy.com>
2022-08-27 19:43:53 +1000

    Update New-LocalPackage.ps1
    Update for App.json

--- scripts/New-LocalPackage.ps1
+++ scripts/New-LocalPackage.ps1
@@ -1,3 +1,4 @@
+#Requires -PSEdition Desktop
 #Requires -Modules Evergreen, VcRedist
 <#
     Import application packages into Intune locally
@@ -10,48 +11,64 @@ param (
     [System.String] $Path = "E:\projects\packagefactory",
 
     [Parameter()]
-    [System.String[]] $Applications = @("MicrosoftVcRedist2022x86", "MicrosoftVcRedist2022x64", "AdobeAcrobatReaderDC")
+    [System.String] $PackageFolder = "packages",
+
+    [Parameter()]
+    [System.String] $PackageManifest = "App.json",
+
+    [Parameter()]
+    [System.String[]] $Applications = @("Microsoft.NET",
+        "MicrosoftVcRedist2022x86",
+        "MicrosoftVcRedist2022x64",
+        "AdobeAcrobatReaderDC",
+        "ImageCustomise")
 )
 
 foreach ($Application in $Applications) {
-
-    # Get the application details
-    $Apps = Get-Content -Path "$Path\Applications.json" | ConvertFrom-Json
-    $Filter = ($Apps | Where-Object { $_.Name -eq "$Application" }).Filter
-
+    try {
+        # Get the application details
+        $AppPath = [System.IO.Path]::Combine($Path, $PackageFolder, $Application)
+        $Manifest = Get-Content -Path $([System.IO.Path]::Combine($AppPath, $PackageManifest)) | ConvertFrom-Json
+    }
+    catch {
+        throw $_
+    }
+    
     # Download the application installer
-    if ($Null -ne $Filter) {
-        if ($Filter -match "Get-VcList") {
-            Write-Host "Filter: $Filter"
-            $App = Invoke-Expression -Command $Filter
+    if ($Null -eq $Manifest.Application.Filter) {
+        Write-Host -ForegroundColor "Cyan" "$Application not supported for automatic download."
+    }
+    else {
+        if ($Manifest.Application.Filter -match "Get-VcList") {
+
+            Write-Host -ForegroundColor "Cyan" "Filter: $Manifest.Application.Filter"
+            $App = Invoke-Expression -Command $Manifest.Application.Filter
             $Filename = $(Split-Path -Path $App.Download -Leaf)
-            Write-Host "Package: $($App.Name); $Filename."
-            New-Item -Path "$Path\packages\$Application\Source" -ItemType "Directory" -Force | Out-Null
-            Invoke-WebRequest -Uri $App.Download -OutFile "$Path\packages\$Application\Source\$Filename" -UseBasicParsing
+            Write-Host -ForegroundColor "Cyan" "Package: $($App.Name); $Filename."
+            New-Item -Path [System.IO.Path]::Combine($AppPath, $Manifest.PackageInformation.SourceFolder) -ItemType "Directory" -Force | Out-Null
+            Invoke-WebRequest -Uri $App.Download -OutFile [System.IO.Path]::Combine($AppPath, $Manifest.PackageInformation.SourceFolder, $Filename) -UseBasicParsing
         }
         else {
-            Write-Host "Filter: $Filter"
-            $result = Invoke-Expression -Command $Filter | Save-EvergreenApp -CustomPath "$Path\packages\$Application\Source"
+
+            Write-Host -ForegroundColor "Cyan" "Filter: $Manifest.Application.Filter"
+            $result = Invoke-Expression -Command $Manifest.Application.Filter | Save-EvergreenApp -CustomPath [System.IO.Path]::Combine($AppPath, $Manifest.PackageInformation.SourceFolder)
             if ($result.FullName -match "\.zip$") {
-                Expand-Archive -Path $result.FullName -DestinationPath "$Path\packages\$Application\Source" -Force
+                Expand-Archive -Path $result.FullName -DestinationPath [System.IO.Path]::Combine($AppPath, $Manifest.PackageInformation.SourceFolder) -Force
                 Remove-Item -Path $result.FullName -Force
             }
         }
     }
-    else {
-        Write-Host -ForegroundColor "Cyan" "$Application not supported for automatic download."
-    }
 
     # Package the application
-    if (Test-Path -Path "$Path\packages\$Application\Source") {
+    if (Test-Path -Path [System.IO.Path]::Combine($AppPath, $Manifest.PackageInformation.SourceFolder)) {
         $params = @{
             Application       = $Application
-            Path              = "$Path\packages"
-            DisplayNameSuffix = ""
+            Path              = [System.IO.Path]::Combine($Path, $PackageFolder)
+            DisplayNameSuffix  = ""
         }
         .\Create-Win32App.ps1 @params
     }
     else {
-        Write-Error -Message "Cannot find path $("$Path\packages\$Application\Source")"
+        Write-Error -Message "Cannot find path $([System.IO.Path]::Combine($AppPath, $Manifest.PackageInformation.SourceFolder))"
     }
 }
