cb167df3883e1276cf997ee36cb7882ca64def4f
Aaron Parker <aaron@stealthpuppy.com>
2022-09-25 12:01:34 +1000

    Add Zoom Rooms

---
+++ packages/Apps/ZoomRooms/Source/Uninstall.ps1
@@ -0,0 +1,41 @@
+<#
+    .SYNOPSIS
+    Uninstalls the application
+#>
+[CmdletBinding()]
+param ()
+
+#region Restart if running in a 32-bit session
+if (!([System.Environment]::Is64BitProcess)) {
+    if ([System.Environment]::Is64BitOperatingSystem) {
+        $Arguments = "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$($MyInvocation.MyCommand.Definition)`""
+        $ProcessPath = $(Join-Path -Path $Env:SystemRoot -ChildPath "\Sysnative\WindowsPowerShell\v1.0\powershell.exe")
+        $params = @{
+            FilePath     = $ProcessPath
+            ArgumentList = $Arguments
+            Wait         = $True
+            WindowStyle  = "Hidden"
+        }
+        Start-Process @params
+        exit 0
+    }
+}
+#endregion
+
+try {
+    $App = Get-CimInstance -Class "Win32_Product" | Where-Object { $_.Caption -like "Zoom*" }
+    $params = @{
+        FilePath     = "$Env:SystemRoot\System32\msiexec.exe"
+        ArgumentList = "/uninstall `"$($App.IdentifyingNumber)`" /quiet"
+        NoNewWindow  = $True
+        PassThru     = $True
+        Wait         = $True
+    }
+    $result = Start-Process @params
+}
+catch {
+    throw $_
+}
+finally {
+    exit $result.ExitCode
+}
