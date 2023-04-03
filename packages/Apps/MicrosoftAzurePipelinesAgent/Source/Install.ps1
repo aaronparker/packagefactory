9787cd908e3e6cbd63a08e340370161a21e425c4
Aaron Parker <aaron@stealthpuppy.com>
2023-04-03 19:43:15 +1000

    Update AzureDevOpsPipleineAgent
    Update scripts to support secrets deployed to the machine in a seperate package

--- packages/Apps/MicrosoftAzurePipelinesAgent/Source/Install.ps1
+++ packages/Apps/MicrosoftAzurePipelinesAgent/Source/Install.ps1
@@ -1,13 +1,77 @@
-#description: Installs the Microsoft Azure Pipelines agent to enable automated testing via Azure Pipelines. Do not run on production session hosts.
-#execution mode: Combined
-#tags: Evergreen, Testing, DevOps
-#Requires -Modules Evergreen
-[System.String] $Path = "$Env:SystemDrive\agents"
+#Requires -PSEdition Desktop
+#Requires -RunAsAdministrator
+<#
+    Uses Evergreen to download and install Azure Devops Pipeline agent
+#>
+[CmdletBinding()]
+param (
+    [System.String] $Path = "$Env:SystemDrive\agents",
+    [System.String] $KeyFile = "$Env:SystemRoot\System32\config\systemprofile\AppData\Local\DevOpsKey.xml"
+)
 
-# Check that the required variables have been set in Nerdio Manager
-foreach ($Value in "DevOpsUrl", "DevOpsPat", "DevOpsPool", "DevOpsUser", "DevOpsPassword") {
-    if ($null -eq $SecureVars.$Value) { throw "$Value is $null" }
+#region Restart if running in a 32-bit session
+if (!([System.Environment]::Is64BitProcess)) {
+    if ([System.Environment]::Is64BitOperatingSystem) {
+        $Arguments = "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$($MyInvocation.MyCommand.Definition)`""
+        $ProcessPath = $(Join-Path -Path $Env:SystemRoot -ChildPath "\Sysnative\WindowsPowerShell\v1.0\powershell.exe")
+        $params = @{
+            FilePath     = $ProcessPath
+            ArgumentList = $Arguments
+            Wait         = $True
+            WindowStyle  = "Hidden"
+        }
+        Start-Process @params
+        exit 0
+    }
 }
+#endregion
+
+#region Read secrets file
+if (Test-Path -Path $KeyFile) {
+    continue
+}
+else {
+    throw [System.IO.FileNotFoundException]::new("$KeyFile not found.")
+}
+try {
+    $Secrets = Import-Clixml -Path $KeyFile
+}
+catch {
+    throw $_
+}
+
+# Check that the required variables have been set in the key file
+foreach ($Value in "DevOpsUrl", "DevOpsPat", "DevOpsPool", "DevOpsUser", "DevOpsPassword") {
+    if ($null -eq $Secrets.$Value) { throw "$Value is $null" }
+}
+#endregion
+
+#region Script logic
+# Trust the PSGallery for modules
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-PackageProvider -Name "NuGet" -MinimumVersion "2.8.5.208" -Force -ErrorAction "SilentlyContinue"
+Install-PackageProvider -Name "PowerShellGet" -MinimumVersion "2.2.5" -Force -ErrorAction "SilentlyContinue"
+if (Get-PSRepository | Where-Object { $_.Name -eq "PSGallery" -and $_.InstallationPolicy -ne "Trusted" }) {
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy "Trusted"
+}
+
+# Install the Evergreen module; https://github.com/aaronparker/Evergreen
+$InstalledModule = Get-Module -Name "Evergreen" -ListAvailable -ErrorAction "SilentlyContinue" | `
+    Sort-Object -Property @{ Expression = { [System.Version]$_.Version }; Descending = $true } -ErrorAction "SilentlyContinue" | `
+    Select-Object -First 1
+$PublishedModule = Find-Module -Name "Evergreen" -ErrorAction "SilentlyContinue"
+if (($null -eq $InstalledModule) -or ([System.Version]$PublishedModule.Version -gt [System.Version]$InstalledModule.Version)) {
+    Write-Information -MessageData ":: Install module: "Evergreen"" -InformationAction "Continue"
+    $params = @{
+        Name               = "Evergreen"
+        SkipPublisherCheck = $true
+        Scope              = "CurrentUser"
+        Force              = $true
+        ErrorAction        = "Stop"
+    }
+    Install-Module @params
+}
+#endregion
 
 #region Script logic
 New-Item -Path $Path -ItemType "Directory" -Force -ErrorAction "SilentlyContinue" | Out-Null
@@ -27,14 +91,14 @@ catch {
 try {
     # Create the local account that the DevOps Pipelines agent service will run under
     $params = @{
-        Name                     = $SecureVars.DevOpsUser
-        Password                 = (ConvertTo-SecureString -String $SecureVars.DevOpsPassword -AsPlainText -Force)
+        Name                     = $([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Secrets.DevOpsUser)))
+        Password                 = $(ConvertTo-SecureString -String $([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Secrets.DevOpsPassword))) -AsPlainText -Force)
         Description              = "Azure Pipelines agent service for elevated exec."
         UserMayNotChangePassword = $true
-        Confirm                  = $false
+        Confirm                   = $false
     }
     New-LocalUser @params
-    Add-LocalGroupMember -Group "Administrators" -Member $SecureVars.DevOpsUser
+    Add-LocalGroupMember -Group "Administrators" -Member $DevOpsUser
 }
 catch {
     throw $_
@@ -46,14 +110,14 @@ try {
 
     # Agent install options
     $Options = "--unattended
-        --url `"$($SecureVars.DevOpsUrl)`"
+        --url `"$([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Secrets.DevOpsUrl)))`"
         --auth pat
-        --token `"$($SecureVars.DevOpsPat)`"
-        --pool `"$($SecureVars.DevOpsPool)`"
+        --token `"$([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Secrets.DevOpsPat)))`"
+        --pool `"$([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Secrets.DevOpsPool)))`"
         --agent $Env:COMPUTERNAME
         --runAsService
-        --windowsLogonAccount `"$($SecureVars.DevOpsUser)`"
-        --windowsLogonPassword `"$($SecureVars.DevOpsPassword)`"
+        --windowsLogonAccount `"$([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Secrets.DevOpsUser)))`"
+        --windowsLogonPassword `"$([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Secrets.DevOpsPassword)))`"
         --replace"
     $params = @{
         FilePath     = "$Path\config.cmd"
--- packages/Apps/MicrosoftAzurePipelinesAgent/Source/Uninstall.ps1
+++ packages/Apps/MicrosoftAzurePipelinesAgent/Source/Uninstall.ps1
@@ -1,15 +1,57 @@
-#description: Uninstalls the Microsoft Azure Pipelines agent.
-#execution mode: Combined
-#tags: Evergreen, Testing, DevOps
-#Requires -Modules Evergreen
-[System.String] $Path = "$Env:SystemDrive\agents"
+#Requires -PSEdition Desktop
+#Requires -RunAsAdministrator
+<#
+    Uses Evergreen to download and install Azure Devops Pipeline agent
+#>
+[CmdletBinding()]
+param (
+    [System.String] $Path = "$Env:SystemDrive\agents",
+    [System.String] $KeyFile = "$Env:SystemRoot\System32\config\systemprofile\AppData\Local\DevOpsKey.xml"
+)
+
+#region Restart if running in a 32-bit session
+if (!([System.Environment]::Is64BitProcess)) {
+    if ([System.Environment]::Is64BitOperatingSystem) {
+        $Arguments = "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$($MyInvocation.MyCommand.Definition)`""
+        $ProcessPath = $(Join-Path -Path $Env:SystemRoot -ChildPath "\Sysnative\WindowsPowerShell\v1.0\powershell.exe")
+        $params = @{
+            FilePath     = $ProcessPath
+            ArgumentList = $Arguments
+            Wait         = $True
+            WindowStyle  = "Hidden"
+        }
+        Start-Process @params
+        exit 0
+    }
+}
+#endregion
+
+#region Read secrets file
+if (Test-Path -Path $KeyFile) {
+    continue
+}
+else {
+    throw [System.IO.FileNotFoundException]::new("$KeyFile not found.")
+}
+try {
+    $Secrets = Import-Clixml -Path $KeyFile
+}
+catch {
+    throw $_
+}
+
+# Check that the required variables have been set in the key file
+foreach ($Value in "DevOpsUser", "DevOpsPat") {
+    if ($null -eq $Secrets.$Value) { throw "$Value is $null" }
+}
+#endregion
 
 #region Script logic
 try {
     Push-Location -Path $Path
     $params = @{
         FilePath     = "$Path\config.cmd"
-        ArgumentList = "remove --unattended --auth pat --token `"$($SecureVars.DevOpsPat)`""
+        ArgumentList = "remove --unattended --auth pat --token `"$([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Secrets.DevOpsPat)))`""
         Wait         = $true
         NoNewWindow  = $true
         PassThru     = $true
@@ -22,5 +64,5 @@ catch {
 
 # Remove the C:\agents directory and the local user account used by the agent service
 Remove-Item -Path $Path -Recurse -Force -ErrorAction "SilentlyContinue"
-Remove-LocalUser -Name $SecureVars.DevOpsUser -Confirm:$false -ErrorAction "SilentlyContinue"
+Remove-LocalUser -Name $([System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($Secrets.DevOpsUser))) -Confirm:$false -ErrorAction "SilentlyContinue"
 #endregion
