234c74a4dc6a849dbe9937c82a53f46b0350de99
Aaron Parker <aaron@stealthpuppy.com>
2023-05-01 07:44:13 +1000

    Add Current and LTSR

---
+++ packages/App/CitrixWorkspaceAppLTSR/Source/Uninstall.ps1
@@ -0,0 +1,43 @@
+<#
+    .SYNOPSIS
+    Installs the application
+#>
+[CmdletBinding()]
+param ()
+
+#region Restart if running in a 32-bit session
+if (!([System.Environment]::Is64BitProcess)) {
+    if ([System.Environment]::Is64BitOperatingSystem) {
+        $Arguments = "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$($MyInvocation.MyCommand.Definition)`""
+        $ProcessPath = $(Join-Path -Path $Env:SystemRoot -ChildPath "\Sysnative\WindowsPowerShell\v1.0\powershell.exe")
+
+        $params = @{
+            FilePath     = $ProcessPath
+            ArgumentList = $Arguments
+            Wait         = $True
+            WindowStyle  = "Hidden"
+        }
+        Start-Process @params
+        exit 0
+    }
+}
+#endregion
+
+try {
+    New-Item -Path "$env:ProgramData\PackageFactory\Logs" -ItemType "Directory" -ErrorAction "SilentlyContinue" | Out-Null
+    $Uninstaller = Get-ChildItem -Path "${env:ProgramFiles(x86)}\Citrix" -Filter "TrolleyExpress.exe" -Recurse -ErrorAction "SilentlyContinue"
+    $params = @{
+        FilePath     = $Uninstaller.FullName
+        ArgumentList = "/silent /uninstall /cleanup"
+        NoNewWindow  = $True
+        PassThru     = $True
+        Wait         = $True
+    }
+    $result = Start-Process @params
+}
+catch {
+    throw $_
+}
+finally {
+    exit $result.ExitCode
+}
