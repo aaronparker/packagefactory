48df970bf8127276e0a8e60797de2e98588f0e7f
Aaron Parker <aaron@stealthpuppy.com>
2023-06-11 10:54:41 +1000

    Add Notepad++

---
+++ packages/App/Notepad++/Source/Install.ps1
@@ -0,0 +1,42 @@
+<#
+    .SYNOPSIS
+    Installs the application
+#>
+[CmdletBinding()]
+param ()
+
+#region Restart if running in a 32-bit session
+if (!([System.Environment]::Is64BitProcess)) {
+    if ([System.Environment]::Is64BitOperatingSystem) {
+        $Arguments = "-NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$($MyInvocation.MyCommand.Definition)`""
+        $ProcessPath = $(Join-Path -Path $Env:SystemRoot -ChildPath "\Sysnative\WindowsPowerShell\v1.0\powershell.exe")
+        $params = @{
+            FilePath     = $ProcessPath
+            ArgumentList = $Arguments
+            Wait         = $True
+            WindowStyle  = "Hidden"
+        }
+        Start-Process @params
+        exit 0
+    }
+}
+#endregion
+
+try {
+    New-Item -Path "$env:ProgramData\PackageFactory\Logs" -ItemType "Directory" -ErrorAction "SilentlyContinue" | Out-Null
+    $Installer = Get-ChildItem -Path $PWD -Filter "npp*.exe" -Recurse -ErrorAction "SilentlyContinue"
+    $params = @{
+        FilePath     = $Installer.FullName
+        ArgumentList = "/S"
+        NoNewWindow  = $True
+        PassThru     = $True
+        Wait         = $True
+    }
+    $result = Start-Process @params
+}
+catch {
+    throw $_
+}
+finally {
+    exit $result.ExitCode
+}
