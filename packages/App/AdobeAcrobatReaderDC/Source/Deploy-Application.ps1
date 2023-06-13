4eab3ee7f0fbb31a8471819d0d5f7e1e3cbc7654
Aaron Parker <aaron@stealthpuppy.com>
2023-06-13 10:33:58 +1000

    Update Deploy-Application.ps1
    Revert install commands

--- packages/App/AdobeAcrobatReaderDC/Source/Deploy-Application.ps1
+++ packages/App/AdobeAcrobatReaderDC/Source/Deploy-Application.ps1
@@ -206,15 +206,14 @@ Try {
 
         ## <Perform Installation tasks here>
         # Read JSON file for the installer file name
-        # $Install = Get-Content -Path "$scriptDirectory\Files\Install.json" | ConvertFrom-Json
-        $ArgumentList = "-sfx_nu /sALL /rps /l /msi EULA_ACCEPT=YES ENABLE_CHROMEEXT=0 DISABLE_BROWSER_INTEGRATION=1 ENABLE_OPTIMIZATION=YES ADD_THUMBNAILPREVIEW=1 DISABLEDESKTOPSHORTCUT=1"
-        # $params = @{
-        #     Path             = "$scriptDirectory\Files\$($Install.PackageInformation.SetupFile)"
-        #     Parameters       = $ArgumentList
-        #     WorkingDirectory = "$scriptDirectory\Files"
-        # }
-        # Execute-Process @params
-        Execute-Process -File "AcroRdrDCx642300320201_MUI.exe" -Parameters $ArgumentList
+        $Install = Get-Content -Path "$scriptDirectory\Files\Install.json" | ConvertFrom-Json
+        $ArgumentList = " -sfx_nu /sALL /rps /l /msi EULA_ACCEPT=YES ENABLE_CHROMEEXT=0 DISABLE_BROWSER_INTEGRATION=1 ENABLE_OPTIMIZATION=YES ADD_THUMBNAILPREVIEW=1 DISABLEDESKTOPSHORTCUT=1"
+        $params = @{
+            Path             = "$scriptDirectory\Files\$($Install.PackageInformation.SetupFile)"
+            Parameters       = $ArgumentList
+            WorkingDirectory = "$scriptDirectory\Files"
+        }
+        Execute-Process @params
 
         ##*===============================================
         ##* POST-INSTALLATION
