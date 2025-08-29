a69c69bfd4d30f6acc303c07999a75b2dda57dce
Aaron Parker <aaron@stealthpuppy.com>
2025-08-30 09:28:18 +1000

    Simplify function: Get-InstalledSoftware

--- install/Install.psm1
+++ install/Install.psm1
@@ -88,30 +88,19 @@ function Export-MdtVariable {
 }
 
 function Get-InstalledSoftware {
-    [CmdletBinding()]
-    param ()
-    $UninstallKeys = @(
-        "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*",
-        "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
-    )
-
-    $Apps = @()
-    foreach ($Key in $UninstallKeys) {
-        try {
-            $propertyNames = "DisplayName", "DisplayVersion", "Publisher", "UninstallString", "PSPath", "WindowsInstaller", "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize", "SystemComponent"
-            $Apps += Get-ItemProperty -Path $Key -Name $propertyNames -ErrorAction "SilentlyContinue" | `
-                . { process { if ($null -ne $_.DisplayName) { $_ } } } | `
-                Where-Object { $_.SystemComponent -ne 1 } | `
-                Select-Object -Property @{n = "Name"; e = { $_.DisplayName } }, @{n = "Version"; e = { $_.DisplayVersion } }, "Publisher", "UninstallString", @{n = "RegistryPath"; e = { $_.PSPath -replace "Microsoft.PowerShell.Core\\Registry::", "" } }, "PSChildName", "WindowsInstaller", "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize" | `
-                Sort-Object -Property "DisplayName", "Publisher"
-        }
-        catch {
-            throw $_.Exception.Message
-        }
+    $PropertyNames = "DisplayName", "DisplayVersion", "Publisher", "UninstallString", "PSPath", "WindowsInstaller",
+    "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize", "SystemComponent"
+    ("HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*",
+    "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*") | `
+        ForEach-Object {
+        Get-ItemProperty -Path $_ -Name $PropertyNames -ErrorAction "SilentlyContinue" | `
+            . { process { if ($null -ne $_.DisplayName) { $_ } } } | `
+            Where-Object { $_.SystemComponent -ne 1 } | `
+            Select-Object -Property @{n = "Name"; e = { $_.DisplayName } }, @{n = "Version"; e = { $_.DisplayVersion } }, "Publisher",
+        "UninstallString", @{n = "RegistryPath"; e = { $_.PSPath -replace "Microsoft.PowerShell.Core\\Registry::", "" } },
+        "PSChildName", "WindowsInstaller", "InstallDate", "InstallSource", "HelpLink", "Language", "EstimatedSize" | `
+            Sort-Object -Property "Name", "Publisher"
     }
-
-    Remove-PSDrive -Name "HKU" -ErrorAction "SilentlyContinue" | Out-Null
-    return $Apps
 }
 
 function Get-InstallConfig {
